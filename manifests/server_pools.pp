33a45078490f7d3dd25c72fefddbe8b18ff98d9b
“dsoper2” <dsoper@cisco.com>
2017-05-10 19:17:28 -0700

    Storage profiles and server pools now working.

--- manifests/server_pools.pp
+++ manifests/server_pools.pp
@@ -7,7 +7,7 @@ $server_pool_list=undef,
         $server_pool_list.each |$server_pool| {
             notify { "${server_pool['name']}" : }
 	    $server_pool['server_list'].each |$server| {
-	        ucsm_serverpool{ "${server_pool['name']}" :
+	        ucsm_serverpool { "serverPool ${server_pool['name']}" :
 	            policy_name => "${server_pool['name']}",
 	            pooled_servers => [{"slot_id" => "${server['slot_id']}","chassis_id" =>"${server['chassis_id']}"}],
 	            ip => "${ucsm_dockerdc::login_info['ip']}",
